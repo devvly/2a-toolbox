8204ca0ea3636911b3650afffdaa0fd08f0f1e81
Vitalii Linevych <vitalii.linevych@eduki.com>
2025-05-23 18:26:37 -0400

    Fix nginx generation

--- scripts/down.sh
+++ scripts/down.sh
@@ -1,4 +1,3 @@
-# cd to root directory
 PROJECT_ROOT="$(cd "$(dirname "$0")/../../../../" && pwd)"
 cd "$PROJECT_ROOT"
 
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,6 @@ SOURCE_DIR="$(cd "$(dirname "$0")" && pwd)/../docker"
 USE_MAIL=false
 USE_TESTS=false
 
-# Разбор аргументов
 for arg in "$@"; do
     case $arg in
         --mail)
@@ -16,19 +15,18 @@ for arg in "$@"; do
     esac
 done
 
-# Выбор шаблона
 if $USE_MAIL; then
     TEMPLATE="$SOURCE_DIR/docker-compose-dist-mailpit.yml"
 else
     TEMPLATE="$SOURCE_DIR/docker-compose-dist.yml"
 fi
 
-# Генерация docker-compose.yml
 sed "s/\${PROJECT_NAME}/$PROJECT_NAME/g" "$TEMPLATE" > "$(pwd)/docker-compose.yml"
 
-# Генерация docker-compose-tests.yml если нужно
 if $USE_TESTS; then
     sed "s/\${PROJECT_NAME}/$PROJECT_NAME/g" "$SOURCE_DIR/docker-compose-tests-dist.yml" > "$(pwd)/docker-compose-tests.yml"
 fi
+
+sed "s/\${PROJECT_NAME}/$PROJECT_NAME/g" "$SOURCE_DIR/nginx.conf.dist" > "$SOURCE_DIR/nginx.conf"
 
 echo "✅ Created docker-compose file(s) with project name: $PROJECT_NAME"
